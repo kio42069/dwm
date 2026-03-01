6d9a94fa121e7cb157ca82faa4865d40428b76c5
Surat Sathi Samanta <suratsamanta@gmail.com>
2026-03-01 19:05:46 +0530

    whoa

---
+++ autostart.sh
@@ -0,0 +1,4 @@
+#!/bin/sh 
+xwallpaper --zoom ~/garage/dwm/night.jpg
+dwmblocks &
+swayosd-server &
--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -46,7 +46,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define ALTKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -58,27 +59,79 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *slock[] = {
+  "sh", "-c",
+  "slock & systemctl suspend", 
+  NULL
+};
+static const char *librewolf[]  = { "librewolf", NULL };
+static const char *firefoxcmd[]  = { "firefox", "-p", "main", NULL };
+static const char *firefoxaltcmd[] = { "firefox", "-p", "alt", NULL };
+static const char *screenshotcmd[] = { "/bin/sh", "-c", "maim -s | xclip -sel c -t image/png", NULL };
+// static const char *volup[] = { "pamixer", "-i", "5;", "export", "VOL=$(pamixer --get-volume);", "notify-send", "$VOL", NULL };
+static const char *volup[] = {
+    "sh", "-c",
+    "pamixer -i 5; VOL=$(pamixer --get-volume); notify-send -a Volume -r 32 \"$VOL\"",
+    NULL
+};
+// static const char *voldown[] = { "pamixer", "-d", "5", NULL };
+static const char *voldown[] = {
+    "sh", "-c",
+    "pamixer -d 5; VOL=$(pamixer --get-volume); notify-send -a Volume -r 32 \"$VOL\"",
+    NULL
+};
+// static const char *voltog[] = { "pamixer", "-t", NULL };
+static const char *voltog[] = {
+    "sh", "-c",
+    "pamixer -t; MUTE=$(pamixer --get-mute); notify-send -a Volume -r 32 \"$MUTE\"",
+    NULL
+};
+// static const char *brightup[] = { "brightnessctl", "s", "5%+", NULL };
+static const char *brightup[] = {
+    "sh", "-c",
+    "brightnessctl s 5%+; BRIGHT=${brightnessctl -m|cut -d, -f4}; notify-send -a Brightness -r 33 \"$BRIGHT\"",
+    NULL
+};
+// static const char *brightdown[] = { "brightnessctl", "s", "5%-", NULL };
+static const char *brightdown[] = {
+    "sh", "-c",
+    "brightnessctl s 5%-; BRIGHT=${brightnessctl -m|cut -d, -f4}; notify-send -a Brightness -r 33 \"$BRIGHT\"",
+    NULL
+};
+// static const char *mictog[] = { "swayosd-client", "--input-volume", "mute-toggle", NULL };
+static const char *mictog[] = {
+    "sh", "-c",
+    "pactl set-source-mute @DEFAULT_SOURCE@ toggle && pkill -RTMIN+10 dwmblocks;",
+    NULL
+};
+static const char *filemgr[] = { "thunar", NULL };
+static const char *snoop[] = {"systemctl", "suspend", NULL};
+
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_z,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_Down,   focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_Right,  focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_Up,     focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_Left,   focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_o,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_semicolon,setmfact,     {.f = -0.05} },
+	{ MODKEY,                       XK_apostrophe,setmfact,    {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
+  { MODKEY,                       XK_f,      fullscreen,     {0} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -97,6 +150,19 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+  { MODKEY,                       XK_w,      spawn,          { .v = firefoxcmd} },
+  { MODKEY,                       XK_r,      spawn,          { .v = firefoxaltcmd} },
+  { MODKEY,                       XK_l,      spawn,          { .v = slock } },
+  { MODKEY,                       XK_s,      spawn,          { .v = screenshotcmd } },
+  { ALTKEY,                       XK_F1,     spawn,          { .v = voltog } },
+  { ALTKEY,                       XK_F2,     spawn,          { .v = voldown } },
+  { ALTKEY,                       XK_F3,     spawn,          { .v = volup } },
+  { ALTKEY,                       XK_F4,     spawn,          { .v = mictog } },
+  { ALTKEY,                       XK_F5,     spawn,          { .v = brightdown } },
+  { ALTKEY,                       XK_F6,     spawn,          { .v = brightup } },
+  { MODKEY,                       XK_e,      spawn,          { .v = filemgr } },
+  { 0,                            XK_F10,    spawn,          { .v = snoop } },
+  { MODKEY,                       XK_d,      spawn,          { .v = librewolf } },
 };
 
 /* button definitions */
@@ -110,6 +176,7 @@ static const Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY|ShiftMask,Button1,       resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
