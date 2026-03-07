05c192b4372b08dc8e975a70f304baaaab8b9828
Surat Sathi Samanta <suratsamanta@gmail.com>
2026-03-07 20:14:31 +0530

    remove the diff which remembers where the window was spawned hella nnoying

--- config.h
+++ config.h
@@ -8,6 +8,7 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+// static const unsigned int gappx     = 20;        /* gaps between windows */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
 static const char col_gray1[]       = "#222222";
